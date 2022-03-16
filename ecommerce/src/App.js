76736c9e707867cd3feba6a695dc70373e32d560
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-03-16 18:07:56 -0400

    ecommerce pages into react

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import "../src/css/main.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="wrapper">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/products" component={Products} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ ecommerce/src/pages/Home.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import "../css/main.css";
+
+const Home = () => {
+    return(
+        <div>
+        <header class="stretch">
+    <img class="logo" src="img/ecommercelogo.png" alt="logo"/>
+    <h1>Gone Golfing</h1>
+
+    <nav>
+      <ul>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="products.html">Products</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section id="hero">
+    <h2 class="whitesmoke">Golfing is a necessity, grab your gears for your next round!</h2>
+  </section>
+  <section>
+      <h3>Whether you are a beginner, amateur, or pro, you are in the right place! <br /> Check out the products page for more information or if you have any questions just contact us through the contact page. <br />
+      Come and grab your gears for your next round of golf!</h3>
+    </section>
+
+  <footer id="footer">
+    <div class="footer-text">
+      <div class="footer-links">
+        <a href="home.html"> Home |</a>
+        <a href="products.html"> Products </a>
+        <a href="contact.html"> | Contact Us </a>
+      </div>
+      <br />
+      <div class="copyright">
+        <p>
+          &copy; Gone Golfing | ALL RIGHTS RESERVED.
+        </p>
+      </div>
+    </div>
+    <br />
+    <div>
+      <a href="https://www.facebook.com/" target="_blank">
+        <img src="img/facebooklogo.jpeg" alt="facebook" width="50px"/>
+      </a>
+      <a href="https://www.instagram.com/" target="_blank">
+        <img src="img/instagramlogo.jpeg" alt="instagram" width="50px"/>
+      </a>
+      <a href="https://www.twitter.com/" target="_blank">
+        <img src="img/twitterlogo.png" alt="instagram" width="50px"/>
+      </a>
+    </div>
+  </footer>
+        </div>
+    );
+}
