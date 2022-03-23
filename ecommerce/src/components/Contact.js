880d62195c3c91bc6f87ea221f42f09bbd394f38
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-03-23 11:45:29 -0400

    contact form fully validated

--- ecommerce/src/components/Contact.js
+++ ecommerce/src/components/Contact.js
@@ -7,7 +7,10 @@ export default function Contact() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [errorFirstName, setErrorFirstName] = useState(null);
+  const [errorLastName, setErrorLastName] = useState(null);
+  const [errorEmail, setErrorEmail] = useState(null);
+  const [submitted, setSubmitted] = useState(null);
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
     useEffect(() => {
       if (firstName && lastName && email) {
@@ -21,18 +24,26 @@ export default function Contact() {
           console.log(validName.test(firstName))
           console.log(validName.test(lastName))
           console.log(validEmail.test(email))
-
+          setSubmitted(null);
           if (!validName.test(firstName)){
-              setErrorMessage('Invalid First Name, try again!');
+              setErrorFirstName('Invalid First Name, try again!');
             } 
-            if (!validName.test(lastName)) {
-              setErrorMessage('Invalid Last Name, try again!');
+            else if(!validName.test(lastName)) {
+              setErrorFirstName(null);
+              setErrorLastName('Invalid Last Name, try again!');
             }
-            if (!validEmail.test(email)) {
-              setErrorMessage('Invalid Email, try again!');
+            else if (!validEmail.test(email)) {
+              setErrorLastName(null);
+              setErrorEmail('Invalid Email, try again!');
             }
             else {
-                setErrorMessage(null);
+              setErrorLastName(null);
+              setErrorFirstName(null);
+              setErrorEmail(null);
+                setSubmitted("Thank you! Your information was received and we will reach out to you as soon as possible!");
+                setFirstName("");
+                setLastName("");
+                setEmail("");
                 return
             }
             return 
@@ -43,16 +54,18 @@ export default function Contact() {
   <form>
       <label htmlFor="first">First name:</label><br />
       <input type="text" id="first" name="first" placeholder="John" value={firstName} onChange={e => setFirstName(e.target.value)}/><br />
+      {errorFirstName ? (<div className="red">{errorFirstName}</div>) : null}
       <label htmlFor="last">Last name:</label><br />
       <input type="text" id="last" name="last" placeholder="Doe" value={lastName} onChange={e => setLastName(e.target.value)}/><br />
-      <label htmlFor="phone">Phone Number:</label><br />
-      <input type="tel" id="phone" name="phone" placeholder="555-555-5555" /><br />
+      {errorLastName ? (<div className="red">{errorLastName}</div>) : null}
       <label htmlFor="email">E-mail:</label><br />
       <input type="email" id="email" name="email" placeholder="JohnDoe@gmail.com" value={email} onChange={e => setEmail(e.target.value)}/><br />
+      {errorEmail ? (<div className="red">{errorEmail}</div>) : null}
       <label htmlFor="comments">Comments:</label><br />
-      <textarea name="comments" id="comments" placeholder="Leave a question or comment here..."></textarea> <br />
-      {errorMessage ? (<div>{errorMessage}</div>) : null}
-      <button disabled={isButtonDisabled} onSubmit={handleSubmit} formAction="#">Submit</button>
+      <textarea name="comments" id="comments" placeholder="Leave any questions or comments here..."></textarea> <br />
+      {submitted ? (<div className="red">{submitted}</div>) : null}
+      <button disabled={isButtonDisabled} onClick={handleSubmit} formAction="#">Submit</button>
+      
   </form>
 
         </div>
